f16a4caa8128b799099e5318ad14a42a077eab35
Ardhi Lukianto <ardhi@lukianto.com>
2024-07-11 09:16:22 +0700

    Refactor: handler -> readHandler

--- bajo/config-handlers.js
+++ bajo/config-handlers.js
@@ -2,18 +2,18 @@ import yaml from 'js-yaml'
 import toml from '@iarna/toml'
 import fs from 'fs'
 
-const yamlHandler = async function (file, isContent) {
+const yamlReadHandler = async function (file, isContent) {
   const content = isContent ? file : fs.readFileSync(file, 'utf8')
   return yaml.load(content)
 }
 
-const tomlHandler = async function (file, isContent) {
+const tomlReadHandler = async function (file, isContent) {
   const content = isContent ? file : fs.readFileSync(file, 'utf8')
   return toml.parse(content)
 }
 
 export default [
-  { ext: '.yml', handler: yamlHandler },
-  { ext: '.yaml', handler: yamlHandler },
-  { ext: '.toml', handler: tomlHandler }
+  { ext: '.yml', readHandler: yamlReadHandler },
+  { ext: '.yaml', readHandler: yamlReadHandler },
+  { ext: '.toml', readHandler: tomlReadHandler }
 ]
