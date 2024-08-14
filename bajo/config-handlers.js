a6d32116bcf886841776c17a9759dcdb737d6135
Ardhi Lukianto <ardhi@lukianto.com>
2024-08-14 07:58:08 +0700

    Add writeHandlers

--- bajo/config-handlers.js
+++ bajo/config-handlers.js
@@ -1,19 +1,26 @@
-import yaml from 'js-yaml'
-import toml from '@iarna/toml'
-import fs from 'fs'
+import fromToml from './method/from-toml.js'
+import fromYaml from './method/from-yaml.js'
+import toYaml from './method/to-yaml.js'
+import toToml from './method/to-toml.js'
 
 const yamlReadHandler = async function (file, isContent) {
-  const content = isContent ? file : fs.readFileSync(file, 'utf8')
-  return yaml.load(content)
+  return fromYaml(file, isContent)
+}
+
+const yamlWriteHandler = async function (file, isContent) {
+  return toYaml(file, isContent)
 }
 
 const tomlReadHandler = async function (file, isContent) {
-  const content = isContent ? file : fs.readFileSync(file, 'utf8')
-  return toml.parse(content)
+  return fromToml(file, isContent)
+}
+
+const tomlWriteHandler = async function (file, isContent) {
+  return toToml(file, isContent)
 }
 
 export default [
-  { ext: '.yml', readHandler: yamlReadHandler },
-  { ext: '.yaml', readHandler: yamlReadHandler },
-  { ext: '.toml', readHandler: tomlReadHandler }
+  { ext: '.yml', readHandler: yamlReadHandler, writeHandler: yamlWriteHandler },
+  { ext: '.yaml', readHandler: yamlReadHandler, writeHandler: yamlWriteHandler },
+  { ext: '.toml', readHandler: tomlReadHandler, writeHandler: tomlWriteHandler }
 ]
