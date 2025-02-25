7e670b1dbef71bd6c5ab3abbda4440f66caf7945
Ardhi Lukianto <ardhi@lukianto.com>
2025-02-25 09:14:11 +0700

    Updates

--- bajo/config-handlers.js
+++ bajo/config-handlers.js
@@ -4,18 +4,22 @@ import toYaml from './method/to-yaml.js'
 import toToml from './method/to-toml.js'
 
 const yamlReadHandler = async function (file, opts = {}) {
+  if (opts === true) opts = { isContent: true }
   return fromYaml(file, opts.isContent)
 }
 
 const yamlWriteHandler = async function (file, opts = {}) {
+  if (opts === true) opts = { isContent: true }
   return toYaml(file, opts.isContent)
 }
 
 const tomlReadHandler = async function (file, opts = {}) {
+  if (opts === true) opts = { isContent: true }
   return fromToml(file, opts.isContent)
 }
 
 const tomlWriteHandler = async function (file, opts = {}) {
+  if (opts === true) opts = { isContent: true }
   return toToml(file, opts.isContent)
 }
 
